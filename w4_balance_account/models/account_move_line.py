15896cb10463bdbd6fe69adbb7b6fada66c90427
w4odoo15 <147035907+w4odoo15@users.noreply.github.com>
2025-09-18 20:44:41 +0200

    tr

--- w4_balance_account/models/account_move_line.py
+++ w4_balance_account/models/account_move_line.py
@@ -1,66 +1,53 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 from odoo import models, fields, api, _
-from odoo.tools import get_lang
+from odoo.tools import get_lang, SQL
 
 class AccountMoveLine(models.Model):
     _inherit = "account.move.line"
 
-    bal_acc = fields.Char(compute="_compute_bal_acc", store=True, string="G-Konto")
+    bal_acc = fields.Char(compute="_compute_bal_acc",store=True)
     
-    @api.depends('debit', 'credit', 'move_id.state')
+    @api.depends('debit','credit','move_id','move_id.state')
     def _compute_bal_acc(self):
         for rec in self:
             bal_acc = 'div'
             if rec.move_id and rec.debit > 0.0:
-                line = self.env['account.move.line'].search([
-                    ('move_id', '=', rec.move_id.id),
-                    ('id', '!=', rec.id),
-                    ('credit', '=', rec.debit)
-                ], limit=1)
+                line = self.env['account.move.line'].search([('move_id','=',rec.move_id.id),('id','!=',rec.id),('credit','=',rec.debit)],limit=1)
                 if line:
-                    bal_acc = line.account_id.code
+                   bal_acc = line.account_id.code 
+                else:
+                    bal_acc = 'div'
             elif rec.move_id and rec.credit > 0.0:
-                line = self.env['account.move.line'].search([
-                    ('move_id', '=', rec.move_id.id),
-                    ('id', '!=', rec.id),
-                    ('debit', '=', rec.credit)
-                ], limit=1)
+                line = self.env['account.move.line'].search([('move_id','=',rec.move_id.id),('id','!=',rec.id),('debit','=',rec.credit)],limit=1)
                 if line:
-                    bal_acc = line.account_id.code
+                   bal_acc = line.account_id.code
+                else:
+                    bal_acc = 'div'
             rec.bal_acc = bal_acc
 
 class GeneralLedgerCustomHandler(models.AbstractModel):
     _inherit = 'account.general.ledger.report.handler'
 
     def _get_query_amls(self, report, options, expanded_account_ids, offset=0, limit=None):
-        """ Construct a query retrieving the account.move.lines when expanding a report line with or without the load
-        more.
-        :param options:               The report options.
-        :param expanded_account_ids:  The account.account ids corresponding to consider. If None, match every account.
-        :param offset:                The offset of the query (used by the load more).
-        :param limit:                 The limit of the query (used by the load more).
-        :return:                      (query, params)
-        """
+        """ Construct a query retrieving the account.move.lines when expanding a report line with or without the load more. """
         additional_domain = [('account_id', 'in', expanded_account_ids)] if expanded_account_ids is not None else None
         queries = []
-        all_params = []
-        lang = self.env.user.lang or get_lang(self.env).code
-        journal_name = f"COALESCE(journal.name->>'{lang}', journal.name->>'en_US')" if \
-            self.pool['account.journal'].name.translate else 'journal.name'
-        account_name = f"COALESCE(account.name->>'{lang}', account.name->>'en_US')" if \
-            self.pool['account.account'].name.translate else 'account.name'
+        journal_name = self.env['account.journal']._field_to_sql('journal', 'name')
         for column_group_key, group_options in report._split_options_per_column_group(options).items():
-            # Get sums for the account move lines.
-            # period: [('date' <= options['date_to']), ('date', '>=', options['date_from'])]
-            tables, where_clause, where_params = report._query_get(group_options, domain=additional_domain, date_scope='strict_range')
-            ct_query = report._get_query_currency_table(group_options)
-            query = f'''
-                (SELECT
+            query = report._get_report_query(group_options, domain=additional_domain, date_scope='strict_range')
+            account_alias = query.left_join(lhs_alias='account_move_line', lhs_column='account_id', rhs_table='account_account', rhs_column='id', link='account_id')
+            account_code = self.env['account.account']._field_to_sql(account_alias, 'code', query)
+            account_name = self.env['account.account']._field_to_sql(account_alias, 'name')
+            account_type = self.env['account.account']._field_to_sql(account_alias, 'account_type')
+
+            sql_query = SQL(
+                '''
+                SELECT
                     account_move_line.id,
                     account_move_line.date,
                     account_move_line.date_maturity,
-                    account_move_line.name,
+                    account_move_line.name AS name,
                     account_move_line.ref,
                     account_move_line.company_id,
                     account_move_line.account_id,
@@ -69,45 +56,52 @@ class GeneralLedgerCustomHandler(models.AbstractModel):
                     account_move_line.currency_id,
                     account_move_line.amount_currency,
                     COALESCE(account_move_line.invoice_date, account_move_line.date) AS invoice_date,
-                    ROUND(account_move_line.debit * currency_table.rate, currency_table.precision) AS debit,
-                    ROUND(account_move_line.credit * currency_table.rate, currency_table.precision) AS credit,
-                    ROUND(account_move_line.balance * currency_table.rate, currency_table.precision) AS balance,
+                    %(debit_select)s  AS debit,
+                    %(credit_select)s AS credit,
+                    %(balance_select)s AS balance,
                     move.name AS move_name,
                     company.currency_id AS company_currency_id,
                     account_move_line.bal_acc AS bal_acc,
                     partner.name AS partner_name,
                     move.move_type AS move_type,
-                    account.code AS account_code,
-                    {account_name} AS account_name,
+                    %(account_code)s AS account_code,
+                    %(account_name)s AS account_name,
+                    %(account_type)s AS account_type,
                     journal.code AS journal_code,
-                    {journal_name} AS journal_name,
+                    %(journal_name)s AS journal_name,
                     full_rec.id AS full_rec_name,
-                    %s AS column_group_key
-                FROM {tables}
+                    %(column_group_key)s AS column_group_key
+                FROM %(table_references)s
                 JOIN account_move move ON move.id = account_move_line.move_id
-                LEFT JOIN {ct_query} ON currency_table.company_id = account_move_line.company_id
+                %(currency_table_join)s
                 LEFT JOIN res_company company ON company.id = account_move_line.company_id
                 LEFT JOIN res_partner partner ON partner.id = account_move_line.partner_id
                 LEFT JOIN account_account account ON account.id = account_move_line.account_id
                 LEFT JOIN account_journal journal ON journal.id = account_move_line.journal_id
                 LEFT JOIN account_full_reconcile full_rec ON full_rec.id = account_move_line.full_reconcile_id
-                WHERE {where_clause}
-                ORDER BY account_move_line.date, account_move_line.move_name, account_move_line.id)
-            '''
+                WHERE %(search_condition)s
+                ORDER BY account_move_line.date, move.name, account_move_line.id
+                ''',
+                account_code=account_code,
+                account_name=account_name,
+                account_type=account_type,
+                journal_name=journal_name,
+                column_group_key=column_group_key,
+                table_references=query.from_clause,
+                currency_table_join=report._currency_table_aml_join(group_options),
+                debit_select=report._currency_table_apply_rate(SQL("account_move_line.debit")),
+                credit_select=report._currency_table_apply_rate(SQL("account_move_line.credit")),
+                balance_select=report._currency_table_apply_rate(SQL("account_move_line.balance")),
+                search_condition=query.where_clause,
+            )
+            queries.append(sql_query)
 
-            queries.append(query)
-            all_params.append(column_group_key)
-            all_params += where_params
-
-        full_query = " UNION ALL ".join(queries)
+        full_query = SQL(" UNION ALL ").join(SQL("(%s)", query) for query in queries)
 
         if offset:
-            full_query += ' OFFSET %s '
-            all_params.append(offset)
+            full_query = SQL('%s OFFSET %s ', full_query, offset)
         if limit:
-            full_query += ' LIMIT %s '
-            all_params.append(limit)
+            full_query = SQL('%s LIMIT %s ', full_query, limit)
 
-        return (full_query, all_params)
-
+        return full_query
 
