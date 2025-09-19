588829a56d6620f2a26e8e1a0c96ef499e0d62bd
w4odoo15 <147035907+w4odoo15@users.noreply.github.com>
2025-09-19 14:07:35 +0200

    d

--- w4_balance_account/__manifest__.py
+++ w4_balance_account/__manifest__.py
@@ -14,7 +14,7 @@
     'license': 'OPL-1',
     'version': '18.0.0.1',
     'demo': [],
-    'data': [],
+    'data': ["views/data.xml",],
     'installable': True,
     'application': False,
     'images': ['static/description/cover.gif']
